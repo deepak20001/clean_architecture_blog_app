da127e7662be75d8bc6d2149d6bd8be5b9b3d084
Deepak <djamwal10@gmail.com>
2024-08-26 07:46:48 +0530

    dependency injection with get_it

---
+++ lib/init_dependencies.dart
@@ -0,0 +1,84 @@
+import 'package:blog_app/core/secrets/app_secrets.dart';
+import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
+import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+/// What is Dependency Injection?
+/// Dependency injection is a technique whereby one object supplies the dependencies of another object.
+/// A “dependency” here refers to any external resource or service a class needs to perform its function,
+/// such as a database connection, a REST API, shared preferences, or another class.
+/// DI involves providing these dependencies from the outside, making your components more decoupled, maintainable, and flexible.
+
+/// Why Use Dependency Injection in Flutter?
+/// Testability: DI makes it easier to replace actual implementations with mock or test implementations,
+/// facilitating unit testing without relying on real resources.
+/// Decoupling: It promotes the separation of concerns by decoupling the creation of an object from the behavior of its dependencies,
+/// leading to more modular and maintainable code.
+/// Flexibility: With DI, switching between different implementations of a dependency
+/// (e.g., swapping out database providers) can be done with minimal code changes in the dependent classes.
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  _initAuth();
+
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnonKey,
+  );
+
+  /// this will create a single instance of this dependency throughout the life of app
+  serviceLocator.registerLazySingleton(() => supabase.client);
+}
+
+/// In the code provided, the principle of "depend on interface, not on implementation" (also known as Dependency Inversion Principle) is followed,
+/// by using abstract classes or interfaces as the primary means of interaction in the business logic.
+/// This is evident in the use of the AuthRemoteDataSource interface and AuthRepository interface,
+/// which decouple the higher-level logic (like the AuthBloc and UserSignUp classes) from the specific details of how the underlying data operations are implemented.
+///
+/// How the Principle is Followed ?
+/// Interface Definition:
+/// The AuthRemoteDataSource and AuthRepository would typically be defined as abstract classes or interfaces,
+/// specifying the contract (methods) that any implementation must follow.
+/// This allows your business logic to rely on these contracts rather than the specific implementations.
+///
+/// Implementation Classes:
+/// AuthRemoteDataSourceImpl and AuthRepositoryImpl are concrete classes that implement these interfaces.
+/// They provide the actual implementation details (e.g., interacting with Supabase).
+///
+/// Dependency Injection:
+/// In the main() function, AuthRemoteDataSourceImpl and AuthRepositoryImpl are instantiated and passed into higher-level components (UserSignUp, AuthBloc).
+/// These components rely on the interfaces rather than the concrete implementations, even though in this specific instance,
+/// the implementation is being passed directly.
+///
+/// if you want to switch to a different backend (e.g., Firebase), you only need to create new classes that implement the same interfaces (AuthRemoteDataSource and AuthRepository).
+/// Then, you can replace AuthRemoteDataSourceImpl with a FirebaseAuthRemoteDataSource implementation without modifying the business logic that depends on the interface.
+void _initAuth() {
+  serviceLocator.registerFactory<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImpl(
+      serviceLocator<SupabaseClient>(),
+    ),
+  );
+
+  serviceLocator.registerFactory<AuthRepository>(
+    () => AuthRepositoryImpl(
+      serviceLocator<AuthRemoteDataSource>(),
+    ),
+  );
+
+  serviceLocator.registerFactory(
+    () => UserSignUp(
+      serviceLocator<AuthRepository>(),
+    ),
+  );
+
+  serviceLocator.registerLazySingleton(
+    () => AuthBloc(
+      userSignUp: serviceLocator(),
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,19 @@
 import 'package:blog_app/core/constants/constants.dart';
-import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/core/theme/theme.dart';
-import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
-import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
-import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
+import 'package:blog_app/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final supabase = await Supabase.initialize(
-    url: AppSecrets.supabaseUrl,
-    anonKey: AppSecrets.supabaseAnonKey,
-  );
+  await initDependencies();
+
   runApp(MultiBlocProvider(
     providers: [
-      /// In the code provided, the principle of "depend on interface, not on implementation" (also known as Dependency Inversion Principle) is followed,
-      /// by using abstract classes or interfaces as the primary means of interaction in the business logic.
-      /// This is evident in the use of the AuthRemoteDataSource interface and AuthRepository interface,
-      /// which decouple the higher-level logic (like the AuthBloc and UserSignUp classes) from the specific details of how the underlying data operations are implemented.
-      ///
-      /// How the Principle is Followed ?
-      /// Interface Definition:
-      /// The AuthRemoteDataSource and AuthRepository would typically be defined as abstract classes or interfaces,
-      /// specifying the contract (methods) that any implementation must follow.
-      /// This allows your business logic to rely on these contracts rather than the specific implementations.
-      ///
-      /// Implementation Classes:
-      /// AuthRemoteDataSourceImpl and AuthRepositoryImpl are concrete classes that implement these interfaces.
-      /// They provide the actual implementation details (e.g., interacting with Supabase).
-      ///
-      /// Dependency Injection:
-      /// In the main() function, AuthRemoteDataSourceImpl and AuthRepositoryImpl are instantiated and passed into higher-level components (UserSignUp, AuthBloc).
-      /// These components rely on the interfaces rather than the concrete implementations, even though in this specific instance,
-      /// the implementation is being passed directly.
-      ///
-      /// if you want to switch to a different backend (e.g., Firebase), you only need to create new classes that implement the same interfaces (AuthRemoteDataSource and AuthRepository).
-      /// Then, you can replace AuthRemoteDataSourceImpl with a FirebaseAuthRemoteDataSource implementation without modifying the business logic that depends on the interface.
       BlocProvider(
-        create: (_) => AuthBloc(
-          userSignUp: UserSignUp(
-            AuthRepositoryImpl(
-              AuthRemoteDataSourceImpl(supabase.client),
-            ),
-          ),
-        ),
+        create: (_) => serviceLocator<AuthBloc>(),
       ),
     ],
     child: const MyApp(),
